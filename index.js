2d29e2ed697a82557a41fa07922a612962a2f6fd
Laimukas <l.kestauskas@gmail.com>
2022-03-20 19:47:31 +0200

    Pamokos su java pradmenys

---
+++ index.js
@@ -0,0 +1,25 @@
+// skaiciai
+console.log('Hello People')
+/*
+let skaicius = 5
+let kitasSkaicius = 7
+
+kubas = skaicius ** 3
+console.log(kubas)
+*/
+/*
+let vardas = 'Laimutis';
+let surname = 'Kestauskas';
+let gimimoMetai = 1982;
+let dabartiniaiMetai = 2022;
+//let atsakymas = vardas + " " + surname + " yra " + (dabartiniaiMetai - gimimoMetai) + " metu"
+//arba kitas varijantas
+
+let atsakymas = `${vardas} ${surname} - jam yra ${dabartiniaiMetai - gimimoMetai} metu`
+console.log(atsakymas);
+console.log(atsakymas.length);
+let kitasVardas = "Mano vardas yra";
+console.log(kitasVardas.toLowerCase());
+console.log(kitasVardas.toUpperCase());
+console.log(kitasVardas.charAt(0));
+*/

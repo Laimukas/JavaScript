8539e022e685ad4f35b35a2aa700f0cca92b38a8
Laimukas <l.kestauskas@gmail.com>
2022-03-27 12:32:52 +0300

    truputis uzduoties

---
+++ index2.js
@@ -0,0 +1,19 @@
+console.log('Sveiki prisijunge prie JS mokymu')
+//<<< uzduotis >>>
+
+// uzduotis suskikurti objekta kvadrato
+// sukurti du metodus (plotas ir perimetras) kurie priims krastines ilgi
+// kvadrato.plotas(10)  - grazins 100
+// kvadrato.perimetras(10) - 40
+/*
+const kvadrato = {
+plotas: function(length){
+  console.log(length ** 2)
+},
+ perimetras: function(length) {
+  console.log(length * 4)
+}
+}
+kvadrato.plotas(10);
+kvadrato.perimetras(10)
+*/

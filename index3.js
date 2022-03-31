3437aade81e1d653261a13c0242477803673e544
Laimukas <l.kestauskas@gmail.com>
2022-03-31 16:54:35 +0300

    paskutine to do list versija

--- index3.js
+++ index3.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+  let grizedarbai = JSON.parse(localStorage.getItem('darbai'));
+  console.log(grizedarbai)
+};
+
+
 // Global selectors  and variables // 
 const myContainer = document.querySelector('.container');
 let myInput = document.querySelector('input');
@@ -13,17 +19,45 @@ let totalNumberOfTodos = 0;
 let completedTodos = 0;
 let percentOfCompetedTasks = 0;
 
-myBtn.addEventListener('click', function(){
-  
+let myTodosList = [];
 
+myBtn.addEventListener('click', function(){
   let inputText = myInput.value;
 
   if (inputText.length > 0) {
   let myLi = document.createElement('li');
   const checkBoxInput = document.createElement('input');
+  // will use in localStorage 
+  myTodosList.push(
+    {
+      todo: inputText,
+      done: false,
+    }
+  )
+ 
   
   checkBoxInput.setAttribute('type', 'checkbox');
   checkBoxInput.addEventListener('click', (event) => {
+   
+    let jobThatImEditing = event.target.nextElementSibling.textContent
+    
+
+    myTodosList.forEach(job => 
+      job.todo === jobThatImEditing ? job.done = !job.done : job)
+    // job yra kievienas darbas musu masyve 
+    //  -  {todo: inputText, done: false} 
+    // job.todo yra mano li kuriame paspaudziau checkbox tekstas
+    // identiskas variantas uzrasytas su paprasta funkcija
+    // myTodosList.forEach(function(job) {
+    //   if(job.todo === jobThatImEditing) {
+    //     job.done = !job.done
+    //   } else {
+    //     return job
+    //   }
+    // })
+    //
+    console.log(myTodosList)
+
     event.target.parentElement.classList.toggle('atlikta')
     completedTodos = document.querySelectorAll('.atlikta').length
     myProgressLabel.innerHTML = `${completedTodos} of ${totalNumberOfTodos}`
@@ -41,6 +75,8 @@ editIcon.classList.add(...editClassList);
   mySpan.innerText = inputText;
   const todoEditInput = document.createElement('input');
   todoEditInput.setAttribute('class', 'hidden');
+  todoEditInput.setAttribute('type', 'text');
+  // naudosime veliasniam selectoriui - .querySelector('input[type=text]'
   todoEditInput.setAttribute('value', inputText);
 
   function toggleEdit() {
@@ -55,7 +91,21 @@ editIcon.classList.add(...editClassList);
     
   })
 
-  editIcon.addEventListener('click', function() {
+  editIcon.addEventListener('click', function(event) {
+    // naudosiu tam, kad galeciau susirasti job kuri keiciu sename masyve
+    let oldValue = event.target.parentNode.textContent;
+    // sita reiksme perrasysiu senaja reiksme
+    let newValue = event.target.parentNode.querySelector('input[type=text]').value;
+    // einu per savo darbu masyva [ job, job, job] 
+    // kiekvienas job yra nauajs {todo: inputText, done: false} elemntas mano masyve
+    myTodosList.forEach(job => 
+      job.todo === oldValue ? job.todo = newValue : job)
+      // kad gauti value, man reikia nurodyti key kuris naudojamas job objekte
+      
+      // ternary operatoriaus paaiskinimas
+      // salyga ? ka grazinsime jei ji teisinga : ka grazinsime jei neteisinga
+      // ? gali buti isivaizduojamas kaip if
+      // : galime isivaizduoti kaip else
     toggleEdit()
   })
 
@@ -66,6 +116,23 @@ const deleteClassList = ['bi', 'bi-trash-fill'];
 deleteIcon.classList.add(...deleteClassList);
 
 deleteIcon.addEventListener('click', (event) => {
+  // currentValue bus naudojama susrasti darba kuri trinsime
+  let currentValue = event.target.parentNode.textContent;
+  // jei radau dabra kurio todo reiksme sutampa su currentValue paimu savo myTodosList
+  // masyva is is jo pasalinu viena darba tie atitinkamu index
+  myTodosList.forEach((job, index) => 
+  // viduje galioja tas pats job ir index, pasikeitus job keiciasi ir index
+  (job.todo === currentValue ? myTodosList.splice(index, 1) : job) )
+  // ['cat', 'dog', 'frog']
+  // primas ratas -> job = cat, index = 0
+  // antras ratas -> job = dog, index = 1
+  // trecias ratas -> job = frog, index = 2
+
+  // myTodosList.splice(index, 1) Paaiskinimas 
+  // splice pirmas parametras nurodo, elemento, kuri trinsime vieta masyve - index
+  // antras parametras nurodo kiek elementu triname, siuo atveju 1
+  // nenurodzius kiek elementu triname, butu pasalinti visi
+
   event.target.parentElement.remove();
   completedTodos = document.querySelectorAll('.atlikta').length
   totalNumberOfTodos = document.querySelectorAll('li').length;
@@ -91,19 +158,31 @@ myLi.appendChild(editIcon)
 
   myProgressBar.value = percentOfCompetedTasks;
   myProgressLabel.innerHTML = `${completedTodos} of ${totalNumberOfTodos}`
-
-
   myInput.value = ''
+
   } else {
     alert('Iveskite uzduoti')
   }
+  // console.log(myTodosList)
 })
 
+
+
 removeAllBtn.addEventListener('click', function() {
+
+  let newCOmpletedTodos = myTodosList.filter(job => job.done !== true)
+
   let allCompletedTasks = document.querySelectorAll('.atlikta');
   allCompletedTasks.forEach(item => item.remove());
   totalNumberOfTodos = document.querySelectorAll('li').length;
   completedTodos = 0
   myProgressLabel.innerHTML = `${completedTodos} of ${totalNumberOfTodos}`
   myProgressBar.value = 0;
+  myTodosList = [...newCOmpletedTodos]
+  // ... dar vadinamas spread operatoriumi
+})
+
+
+window.addEventListener("beforeunload", function(){
+  localStorage.setItem('darbai', JSON.stringify(myTodosList))
 })
